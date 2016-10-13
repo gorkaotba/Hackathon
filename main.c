99f9fa499c41220493cc811441563c4b4efa92c1
Gorka Otermin <Gorka Otermin>
2016-10-13 10:03:26 +0200

    funciones

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
     printf("Hello world!\n");
+
     return 0;
 }
+
+int readCAT (){
+//Identificar qué tipo de DATA ITEM viene y llamar a las funciones, CAT
+
+}
+
+int readLENGTH (){
+
+}
+
+int readFSPEC (){
+
+}
+
+int typeIdentyfier (){
+
+
+}
+
+int readFixed (){
+
+}
+
+int readVariable (){
+
+}
+
+int readRepetitive (){
+
+}
+
+int readComponed (){
+
+}
+
+
+
+
+
+
+
